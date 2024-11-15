3255772b2ef798d7965aec5a3cd07763c292aa13
Chocolatito-de-unicornio <>
2024-11-15 11:30:32 -0300

    z
    z

---
+++ scripts.js
@@ -0,0 +1,6 @@
+// Seleccionamos el botón por su ID
+const boton = document.getElementById("mi-boton");
+// Agregamos un evento de clic al botón
+boton.addEventListener("click", function () {
+alert("Prácticamente ya sos ingeniero. *** Silicon Valley te espera.");
+});
